867e49947167b51a1fca9afa4eb3aef655754506
SS1857 <89942518+SS1857@users.noreply.github.com>
2022-04-10 23:00:39 +0530

    Add files via upload

---
+++ GL-Java-FSD-Work-Lab-2/src/com/gl/javafsd/traveller/currencyspend/Main.java
@@ -0,0 +1,12 @@
+package com.gl.javafsd.traveller.currencyspend;
+
+public class Main {
+
+public static void main(String[] args) {
+		
+		TravellerCurrencySpendUI ui = 
+			new TravellerCurrencySpendUI();
+		ui.init();
+	}
+}
+
